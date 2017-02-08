d32c806c8d93ae26c4d9fbbdf5f4465c77f164b8
Grox2006 <thibaud.laurent6@gmail.com>
2017-02-08 01:26:06 +0100

    [Fix] Passage de la vitesse de déplacement de 1 à 4.

--- resources/scripts/player_move.js
+++ resources/scripts/player_move.js
@@ -1,13 +1,5 @@
 new Behaviour("player_move", function(object, delay)
 {
-    var a = Math.floor(object.x);
-    
-    object.x += (Input.get(68) - Input.get(81)) * delay;
-    object.y += (Input.get(83) - Input.get(90)) * delay;
-
-    var b = Math.floor(object.x);
-    if(a != b)
-    {
-        console.log("new tile !");
-    }
+    object.x += (Input.get(68) - Input.get(81)) * delay * 4;
+    object.y += (Input.get(83) - Input.get(90)) * delay * 4;
 });
